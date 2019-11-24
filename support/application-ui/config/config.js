83bf74d20a9cb1bff1962fccc7ff04fc97f0e0f3
jpsdavidson <34160531+jpsdavidson@users.noreply.github.com>
2019-11-24 17:25:07 +0200

    Update config.js

--- support/application-ui/config/config.js
+++ support/application-ui/config/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var ENV = {
-    'kieserver_host' : 'qlb-client-application',
+    'kieserver_host' : 'rhdm7-qlb-loan-kieserver',
     'kieserver_port' : '8080',
     'kieserver_contextroot' : '/kie-server',
     'kieserver_containerId' : 'loan-application_1.0',
